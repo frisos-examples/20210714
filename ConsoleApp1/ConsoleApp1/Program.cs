bf7eb8d6754ac6db3246bb06bcaa98bc4ee503b3
Friso Denijs <friso.artbuff@outlook.com>
2021-07-14 17:20:57 +0200

    added main flow as comments

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,6 +7,23 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            // setup services
+
+
+            // get xml
+            // parse xml
+
+
+            // handle parcels:
+
+            // if weight up to x (1) handle by department y (mail)
+            // if weight up to x (10) handle by department y (regular)
+            // if weight over x (10) handle by department y (heavy)
+
+            // if value more than x (1000) sign of by department y (insurance)
+
+            // if parcel not handled, give warning
         }
     }
 }
